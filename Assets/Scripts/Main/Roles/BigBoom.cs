a67212d904baad6c1f9af2b6a12e05122158c3ce
WannaTwo <33625108+WannaTwo-WeiZhou@users.noreply.github.com>
2018-07-08 10:13:11 +0800

    add big boom

---
+++ Assets/Scripts/Main/Roles/BigBoom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBoom : Enemy
+{
+    protected override void AtkAnimationEvent()
+    {
+        this.Attack();
+
+        base.AtkAnimationEvent();
+
+        this.RemoveSelf();
+    }
+}
